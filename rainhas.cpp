202d38d847708af392f19817dfecba004f9dc29c
RieriUnb <wesleyteixeira0020@gmail.com>
2024-05-08 15:48:48 -0300

    Codigo concertado, estava com 3 erros

--- rainhas.cpp
+++ rainhas.cpp
@@ -1,43 +1,66 @@
-all: testa_rainhas.cpp   rainhas.cpp rainhas.hpp rainhas.o
-	g++ -std=c++11 -Wall rainhas.o testa_rainhas.cpp -o testa_rainhas
-	./testa_rainhas
-	#end
+#include "rainhas.h"
+#include <cmath>
 
-compile: testa_rainhas.cpp   rainhas.cpp rainhas.hpp rainhas.o
-	g++ -std=c++11 -Wall rainhas.o testa_rainhas.cpp -o testa_rainhas
-	
-test: testa_rainhas	
-	./testa_rainhas
-	
-cpplint: testa_rainhas.cpp   rainhas.cpp rainhas.hpp
-	cpplint   --exclude=catch.hpp  *.*
-	
-gcov: testa_rainhas.cpp   rainhas.cpp rainhas.hpp 
-	g++ -std=c++11 -Wall -Wall -fprofile-arcs -ftest-coverage -c rainhas.cpp
-	g++ -std=c++11 -Wall -fprofile-arcs -ftest-coverage rainhas.o testa_rainhas.cpp -o testa_rainhas
-	./testa_rainhas
-	gcov *.cpp	
-	mkdir -p cgcov
-	mv *.gcov *.gcda *.gcno cgcov
-	rm rainhas.o
-	
-debug: testa_rainhas.cpp   rainhas.cpp rainhas.hpp 
-	g++ -std=c++11 -Wall -Wall -g -c rainhas.cpp
-	g++ -std=c++11 -Wall  -g rainhas.o testa_rainhas.cpp -o testa_rainhas
-	gdb testa_rainhas
-	
-	
-cppcheck: testa_rainhas.cpp   rainhas.cpp rainhas.hpp
-	cppcheck  --enable=warning . 
+bool pode_colocar_rainha(char tabuleiro[8][8], int linha, int coluna) {
+    for (int i = 0; i < 8; ++i) {
+        if (tabuleiro[linha][i] == '1') {
+            return false;
+        }
+    }
 
-valgrind: testa_rainhas
-	valgrind --leak-check=full --show-leak-kinds=all ./testa_rainhas
+    for (int i = 0; i < 8; ++i) {
+        if (tabuleiro[i][coluna] == '1') {
+            return false;
+        }
+    }
 
-rainhas.o : rainhas.cpp rainhas.hpp
-	g++ -std=c++11 -Wall -Wall -c rainhas.cpp
-	
-testa_rainhas: 	testa_rainhas.cpp   rainhas.cpp rainhas.hpp rainhas.o
-	g++ -std=c++11 -Wall rainhas.o testa_rainhas.cpp -o testa_rainhas
+    for (int i = linha, j = coluna; i >= 0 && j >= 0; --i, --j) {
+        if (tabuleiro[i][j] == '1') {
+            return false;
+        }
+    }
 
-clean:
-	rm -rf *.o *.exe *.gc*
+    for (int i = linha, j = coluna; i >= 0 && j < 8; --i, ++j) {
+        if (tabuleiro[i][j] == '1') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int verificar_tabuleiro(char tabuleiro[8][8]) {
+    int num_rainhas = 0;
+
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            if (tabuleiro[i][j] != '0' && tabuleiro[i][j] != '1') {
+                return -1; // Entrada inválida
+            }
+            if (tabuleiro[i][j] == '1') {
+                ++num_rainhas;
+                // Verifica se a rainha está atacando outra rainha
+                for (int k = 0; k < 8; ++k) {
+                    if ((k != j && tabuleiro[i][k] == '1') || // Ataque na mesma linha
+                        (k != i && tabuleiro[k][j] == '1')) { // Ataque na mesma coluna
+                        return 0;
+                    }
+                }
+                for (int k = 1; k < 8; ++k) {
+                    if ((i+k < 8 && j+k < 8 && tabuleiro[i+k][j+k] == '1') || // Ataque na diagonal principal
+                        (i-k >= 0 && j-k >= 0 && tabuleiro[i-k][j-k] == '1') || // Ataque na diagonal principal
+                        (i+k < 8 && j-k >= 0 && tabuleiro[i+k][j-k] == '1') || // Ataque na diagonal secundária
+                        (i-k >= 0 && j+k < 8 && tabuleiro[i-k][j+k] == '1')) { // Ataque na diagonal secundária
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+
+    if (num_rainhas != 8) {
+        return -1; // Não há exatamente 8 rainhas
+    }
+
+    return 1; // É uma solução válida
+}
